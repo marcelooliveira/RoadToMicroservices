00ef6229d6dc584203cfab29107db4cabe2947e8
marcelooliveira <mclricardo@gmail.com>
2019-04-13 15:09:48 -0300

    Basket view

---
+++ Part 01/MVC/Controllers/CatalogController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+using System.Diagnostics;
+
+namespace MVC.Controllers
+{
+    public class CatalogController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Part 01/MVC/Startup.cs
+++ Part 01/MVC/Startup.cs
@@ -57,7 +57,7 @@ namespace MVC
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Catalog}/{action=Index}/{id?}");
             });
         }
     }
