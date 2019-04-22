e62cad46c06da5bee645ebffc5807d195ff0fc96
marcelooliveira <mclricardo@gmail.com>
2019-04-22 01:32:16 -0300

    BasketListViewComponent

---
+++ Part 02/MVC/ViewComponents/BasketItemViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models.ViewModels;
+
+namespace MVC.ViewComponents
+{
+    public class BasketItemViewComponent : ViewComponent
+    {
+        public BasketItemViewComponent()
+        {
+        }
+
+        public IViewComponentResult Invoke(BasketItem item)
+        {
+            return View("Default", item);
+        }
+    }
+}
