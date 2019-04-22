734d9da703b1572f0b5c1e82ee558b0f8e660077
marcelooliveira <mclricardo@gmail.com>
2019-04-22 00:39:39 -0300

    Invoke_With_Items_Should_Display_Default_View

---
+++ Part 02/MVC.Test/BasketListViewComponentTest.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using MVC.Models.ViewModels;
+using MVC.ViewComponents;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MVC.Test
+{
+    public class BasketListViewComponentTest
+    {
+        [Fact]
+        public void Invoke_With_Items_Should_Display_Default_View()
+        {
+            //arrange 
+            var vc = new BasketListViewComponent();
+            List<BasketItem> items =
+            new List<BasketItem>
+            {
+                new BasketItem { Id = 1, ProductId = 1, Name = "Broccoli", UnitPrice = 59.90m, Quantity = 2 },
+                new BasketItem { Id = 2, ProductId = 5, Name = "Green Grapes", UnitPrice = 59.90m, Quantity = 3 },
+                new BasketItem { Id = 3, ProductId = 9, Name = "Tomato", UnitPrice = 59.90m, Quantity = 4 }
+            };
+
+            //act 
+            var result = vc.Invoke(items);
+
+            //assert
+            ViewViewComponentResult vvcResult = Assert.IsAssignableFrom<ViewViewComponentResult>(result);
+            Assert.Equal("Default", vvcResult.ViewName);
+        }
+    }
+}
