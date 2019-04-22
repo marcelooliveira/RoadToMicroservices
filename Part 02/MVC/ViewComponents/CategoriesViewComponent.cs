8372cb155e7042c066a217ea6e3f0213ae701001
marcelooliveira <mclricardo@gmail.com>
2019-04-22 16:28:14 -0300

    CategoriesViewComponent

---
+++ Part 02/MVC/ViewComponents/CategoriesViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+using System.Collections.Generic;
+
+namespace MVC.ViewComponents
+{
+    public class CategoriesViewComponent : ViewComponent
+    {
+        public CategoriesViewComponent()
+        {
+        }
+
+        public IViewComponentResult Invoke(List<Product> products)
+        {
+            return View("Default", products);
+        }
+    }
+}
