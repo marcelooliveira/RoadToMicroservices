533cbf408de4c06b19911502194437ee1f7bd64d
marcelooliveira <mclricardo@gmail.com>
2019-04-22 23:38:25 -0300

    UserCounterViewComponent

---
+++ Part 02/MVC/ViewComponents/UserCounterViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models.ViewModels;
+
+namespace MVC.ViewComponents
+{
+    public class UserCounterViewComponent : ViewComponent
+    {
+        public UserCounterViewComponent()
+        {
+
+        }
+
+        public IViewComponentResult Invoke(string title, string controllerName, string cssClass, string icon, string count)
+        {
+            var model = new UserCountViewModel(title, controllerName, cssClass, icon, count);
+            return View("Default", model);
+        }
+    }
+}
