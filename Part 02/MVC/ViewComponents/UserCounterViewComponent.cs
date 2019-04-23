51b7468284fd5cccef70d441b7109e1de5dccce2
marcelooliveira <mclricardo@gmail.com>
2019-04-23 20:49:25 -0300

    NotificationCounter, BasketCounter subclasses

--- Part 02/MVC/ViewComponents/UserCounterViewComponent.cs
+++ Part 02/MVC/ViewComponents/UserCounterViewComponent.cs
@@ -4,14 +4,15 @@ using MVC.Services;
 
 namespace MVC.ViewComponents
 {
-    public enum UserCounterType
-    {
-        Notification = 1,
-        Basket = 2
-    }
 
-    public class UserCounterViewComponent : ViewComponent
+    public abstract class UserCounterViewComponent : ViewComponent
     {
+        protected enum UserCounterType
+        {
+            Notification = 1,
+            Basket = 2
+        }
+
         protected readonly IUserCounterService userCounterService;
 
         public UserCounterViewComponent(IUserCounterService userCounterService)
@@ -19,21 +20,32 @@ namespace MVC.ViewComponents
             this.userCounterService = userCounterService;
         }
 
-        public IViewComponentResult Invoke(string title, string controllerName, string cssClass, string icon, UserCounterType userCounterType)
+        protected IViewComponentResult Invoke(string title, string controllerName, string cssClass, string icon, int count, UserCounterType userCounterType)
         {
-            int count = 0;
-
-            if (userCounterType == UserCounterType.Notification)
-            {
-                count = userCounterService.GetNotificationCount();
-            }
-            else if (userCounterType == UserCounterType.Basket)
-            {
-                count = userCounterService.GetBasketCount();
-            }
-
             var model = new UserCountViewModel(title, controllerName, cssClass, icon, count);
-            return View("Default", model);
+            return View("~/Views/Shared/Components/UserCounter/Default.cshtml", model);
+        }
+    }
+
+    public class NotificationCounterViewComponent : UserCounterViewComponent
+    {
+        public NotificationCounterViewComponent(IUserCounterService userCounterService) : base(userCounterService) { }
+
+        public IViewComponentResult Invoke(string title, string controllerName, string cssClass, string icon)
+        {
+            int count = userCounterService.GetNotificationCount();
+            return Invoke(title, controllerName, cssClass, icon, count, UserCounterType.Notification);
+        }
+    }
+
+    public class BasketCounterViewComponent : UserCounterViewComponent
+    {
+        public BasketCounterViewComponent(IUserCounterService userCounterService) : base(userCounterService) { }
+
+        public IViewComponentResult Invoke(string title, string controllerName, string cssClass, string icon)
+        {
+            int count = userCounterService.GetBasketCount();
+            return Invoke(title, controllerName, cssClass, icon, count, UserCounterType.Basket);
         }
     }
 }
