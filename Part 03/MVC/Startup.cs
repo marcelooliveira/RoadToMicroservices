6936cbe5c4d11ef3082afee740e99672c4a8ff18
marcelooliveira <mclricardo@gmail.com>
2019-05-02 23:09:37 -0300

    Identity added

--- Part 03/MVC/Startup.cs
+++ Part 03/MVC/Startup.cs
@@ -56,6 +56,7 @@ namespace MVC
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseCookiePolicy();
 
             app.UseMvc(routes =>
