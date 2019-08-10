bc0983085890aed42da21195f01d6e5c456097d3
marcelooliveira <mclricardo@gmail.com>
2019-08-10 15:04:49 -0300

    Identity.db

---
+++ Part 04/MVC/Areas/Notification/ViewComponents/UserCounterViewComponent.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Areas.Notification.Services;
+using MVC.Models.ViewModels;
+
+namespace MVC.Areas.Notification.ViewComponents
+{
+    public abstract class UserCounterViewComponent : ViewComponent
+    {
+        protected readonly IUserCounterService userCounterService;
+
+        public UserCounterViewComponent(IUserCounterService userCounterService)
+        {
+            this.userCounterService = userCounterService;
+        }
+
+        protected IViewComponentResult Invoke(string title, string controllerName, string cssClass, string icon, int count)
+        {
+            var model = new UserCountViewModel(title, controllerName, cssClass, icon, count);
+            return View("~/Views/Shared/Components/UserCounter/Default.cshtml", model);
+        }
+    }
+
+    public class NotificationCounterViewComponent : UserCounterViewComponent
+    {
+        public NotificationCounterViewComponent(IUserCounterService userCounterService) : base(userCounterService) { }
+
+        public IViewComponentResult Invoke(string title, string controllerName, string cssClass, string icon)
+        {
+            int count = userCounterService.GetNotificationCount();
+            return Invoke(title, controllerName, cssClass, icon, count);
+        }
+    }
+
+    public class BasketCounterViewComponent : UserCounterViewComponent
+    {
+        public BasketCounterViewComponent(IUserCounterService userCounterService) : base(userCounterService) { }
+
+        public IViewComponentResult Invoke(string title, string controllerName, string cssClass, string icon)
+        {
+            int count = userCounterService.GetBasketCount();
+            return Invoke(title, controllerName, cssClass, icon, count);
+        }
+    }
+}
